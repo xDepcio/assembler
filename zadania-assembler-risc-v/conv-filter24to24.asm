90eb5b1fe25d0a6b10100e4493793f75cbac0737
Aleksander <olek.drwal@gmail.com>
2023-05-01 04:04:41 +0200

    working 24bits ver

--- zadania-assembler-risc-v/conv-filter24to24.asm
+++ zadania-assembler-risc-v/conv-filter24to24.asm
@@ -13,9 +13,9 @@
 
 
 	.data
-fname:	.asciz	"cat-128.bmp"
+fname:	.asciz	"cat-32-24bit.bmp"
 	.align	2
-fileDt:	.space	80
+fileDt:	.space	54
 filtDt:	.half	0, 0, 0xff00, 0, 0, 0, 0xff00, 0xfd80, 0xff00, 0, 0xff00, 0xfd80, 0x2b00, 0xfd80, 0xff00, 0, 0xff00, 0xfd80, 0xff00, 0, 0, 0, 0xff00, 0, 0
 # ^	| 0.0  0.0  -1.0  0.0  0.0 |
 # |	| 0.0 -1.0  -2.5 -1.0  0.0 |
@@ -46,20 +46,20 @@ main:
 	mv	s6, a1		# save img height in s6
 	mul	s9, s5, s6	# save total pixels count to s9
 
-	la	a0, fileDt
-	jal	getBitsPerPixels
-	mv	s4, a1		# save bits per pixel to s4
+#	la	a0, fileDt
+#	jal	getBitsPerPixels
+#	mv	s4, a1		# save bits per pixel to s4
 	
-	la	a0, fileDt
-	jal	getColors
-	mv	s1, a1		# save address of colors in s1
-	mv	s8, a2		# save address reserved for newly created colors in s8
+#	la	a0, fileDt
+#	jal	getColors
+#	mv	s1, a1		# save address of colors in s1
+#	mv	s8, a2		# save address reserved for newly created colors in s8
 	
 	la	a0, fileDt
 	jal	getPixles
 	mv	s2, a0		# save address of original pixels in s2
 	mv	s3, a1		# save address of pixels copy in s3
-	#mv	s8, a2		# save pixels size in bytes to s8
+#	mv	s8, a2		# save pixels size in bytes to s8
 	
 	li	s7, 0		# pixels offset in s7
 	li	s10, 0		# curr pixel X in s10
@@ -197,9 +197,10 @@ convFiltLoop:
 	mv	a5, ra
 	mv	a0, a2
 	mv	a1, a3
+#	mv	a0, s7
 	jal	getValAtPixel
 	mv	ra, a5
-	mv	t6, a0		# load pixel index to t6 - offset accounted
+	mv	t6, a0		# load pixel RGB value to t6
 	
 	la	a0, cSave	# retrieve registers
 	lw	a2, (a0)
@@ -213,9 +214,9 @@ convFiltLoop:
 	lw	t4, 32(a0)
 	lw	t5, 36(a0)
 	
-	slli	a0, t6, 2	# multiply index by 4 to get bytes offset of color, store in a0
-	add	a0, s1, a0	# addres of color in a0
-	lw	t6, (a0)	# load color RGB values to t6
+#	slli	a0, t6, 2	# multiply index by 4 to get bytes offset of color, store in a0
+#	add	a0, s1, a0	# addres of color in a0
+#	lw	t6, (a0)	# load color RGB values to t6
 	
 	# Load filter weight of current pixel
 	li	a0, 2
@@ -381,46 +382,24 @@ skipPixel:
 #	ret
 	
 # --------------------------------------------------
-getValAtPixel:
-	# needs   <- deprecated(pixel offset in a2) pixel X in a0, pixel Y in a1
-	# returns -> color index at pixel in a0
-	mul	a2, a1, s5	# Normalize pixels to offset
-	add	a2, a2, a0	# --||--
+getValAtPixel:		# only modifies (a) registers
+	# needs   <- pixel X in a0, pixel Y in a1
+	# returns -> color RGB value in a0
+	mul	a1, a1, s5
+	add	a1, a1, a0	# pixel offset in a1
 	
-	mul	t0, a2, s4	# save pixelOffset * bitsPerPixel to t0
-	srli	t1, t0, 3	# save starting color byte offset to t1 (bitsOffset / 8)
-	andi	t2, t0, 7	# save remainder from dividing bits offset by 8 to t2 (to find starting bit in monochrome palette)
-	mv	t4, s4
-	li	t5, 8
-	li	t6, 0
-	add	t1, t1, s2
-loadColorByte:
-	lbu	t3, (t1)
-	add	t6, t6, t3
-	addi	t4, t4, -8
-	addi	t1, t1, 1
-	blt	s4, t5, getColorBits
-	ble	t4, zero, retVal
-	slli	t6, t6, 8
-	b	loadColorByte
-getColorBits:
-	li	t0, 1
-	beq	s4, t0, retMono
-	b	ret4bits
-retMono:
-	li	t0, 8
-	sub	t0, t0, t2
-	srl	t6, t6, t0
-	andi	t6, t6, 1
-	b	retVal
-ret4bits:
-	li	t0, 4
-	sub	t0, t0, t2
-	srl	t6, t6, t0
-	andi	t6, t6, 15
-	b	retVal
-retVal:
-	mv	a0, t6
+	slli	a2, a1, 1	# mul by 3 (each pixel is 3 bytes)
+	add	a2, a2, a1	# cd...
+	add	a1, a2, s2	# final address of pixel in a1
+
+	lbu	a0, 2(a1)	# load R
+	slli	a0, a0, 8
+	lbu	a2, 1(a1)	# load G
+	add	a0, a0, a2
+	slli	a0, a0, 8
+	lbu	a2, 0(a1)	# loab B
+	add	a0, a0, a2	# final RGB value in a0
+	
 	ret
 # --------------------------------------------------
 	
@@ -448,47 +427,47 @@ readHeader:
 # --------------------------------------------------
 
 # --------------------------------------------------
-getColors:
+#getColors:
 	# needs   <- address of imageData buffer in a0
 	# returns -> address of used colors list stored in heap memory in a1
-	lh	t0, 46(a0)	# lower half of colors count (32 bit)
-	lh	t1, 48(a0)	# upper half od colours count (32 bit)
-	slli	t1, t1, 16
-	add	t2, t1, t0	# final colors count in t2
+#	lh	t0, 46(a0)	# lower half of colors count (32 bit)
+#	lh	t1, 48(a0)	# upper half od colours count (32 bit)
+#	slli	t1, t1, 16
+#	add	t2, t1, t0	# final colors count in t2
 	
-	li	a7, 9		# this part allocates heap memory to store colors
-	slli	t2, t2, 2	# multiply t2 by 4 (each colors takes 4 bytes) - final colours length in t2
-	mv	a0, t2
-	ecall
-	mv	t3, a0		# save address of colors heap start address in t3
+#	li	a7, 9		# this part allocates heap memory to store colors
+#	slli	t2, t2, 2	# multiply t2 by 4 (each colors takes 4 bytes) - final colours length in t2
+#	mv	a0, t2
+#	ecall
+#	mv	t3, a0		# save address of colors heap start address in t3
 	
-	li	a7, 9
+#	li	a7, 9
 	#li	a0, 16777216	# reserve space for 24bit colors space
-	li	a0, 262144	# reserve space for 16bit colors space (2^16 * 4 = 262144)
-	ecall
-	mv	t4, a0		# save address of emtpy colors start address in t4
+#	li	a0, 262144	# reserve space for 16bit colors space (2^16 * 4 = 262144)
+#	ecall
+#	mv	t4, a0		# save address of emtpy colors start address in t4
 	
-	li	a7, 62		# This part seeks to the start of colors in BMP file
-	mv	a0, s0
-	li	a1, 54
-	li	a2, 0
-	ecall
+#	li	a7, 62		# This part seeks to the start of colors in BMP file
+#	mv	a0, s0
+#	li	a1, 54
+#	li	a2, 0
+#	ecall
 	
-	li	a7, 63		# This part reads colors from BMP file
-	mv	a0, s0
-	mv	a1, t3
-	mv	a2, t2
-	ecall
+#	li	a7, 63		# This part reads colors from BMP file
+#	mv	a0, s0
+#	mv	a1, t3
+#	mv	a2, t2
+#	ecall
 	
-	li	a7, 62		# This part seeks back to the start of BMP file
-	mv	a0, s0
-	li	a1, 0
-	li	a2, 0
-	ecall
+#	li	a7, 62		# This part seeks back to the start of BMP file
+#	mv	a0, s0
+#	li	a1, 0
+#	li	a2, 0
+#	ecall
 	
-	mv	a1, t3
-	mv	a2, t4
-	ret
+#	mv	a1, t3
+#	mv	a2, t4
+#	ret
 # --------------------------------------------------
 
 # --------------------------------------------------
@@ -497,10 +476,10 @@ getPixles:
 	# returns -> address of pixels stored in heap memory in a0,
 	#	  -> address of pixels copy in a1,
 	#	  -> pixels size in a2
-	lhu	t0, 10(a0)	
-	lhu	t1, 12(a0)
-	slli	t1, t1, 16
-	add	t2, t1, t0	# offset to raster data in t2
+#	lhu	t0, 10(a0)	
+#	lhu	t1, 12(a0)
+#	slli	t1, t1, 16
+#	add	t2, t1, t0	# offset to raster data in t2
 	
 	#lhu	t0, 2(a0)	
 	#lhu	t1, 4(a0)
@@ -509,14 +488,16 @@ getPixles:
 	#sub	t3, t3, t2	# pixels size in t3 (filesize - offset to raster data)
 	
 	li	a7, 9		# this part allocates heap memory for pixels
-	mul	t3, s9, s4
-	srli	t3, t3, 3
+	slli	t3, s9, 1	# mul by 3 (each pixel takes 3 bytes)
+	add	t3, t3, s9	# cd...
+#	mul	t3, s9, s4
+#	srli	t3, t3, 3
 	mv	a0, t3
 	ecall
 	mv	t4, a0		# save addres of pixels to t4
 	
 	li	a7, 9		# this part allocates heap memory for copy of pixels for 24bit colors (this one will be rewritten and is gargabe at start)
-	srli	t1, s9, 1
+	slli	t1, s9, 1
 	add	t1, t1, s9	# in t1 space in bytes for 24bit colors
 	mv	a0, t1
 	ecall
@@ -524,7 +505,7 @@ getPixles:
 	
 	li	a7, 62		# this part seeks to start of raster data
 	mv	a0, s0
-	mv	a1, t2
+	li	a1, 54
 	li	a2, 0
 	ecall
 	
@@ -534,11 +515,11 @@ getPixles:
 	mv	a2, t3
 	ecall
 	
-	li	a7, 62		# this part seeks to start of raster data
-	mv	a0, s0
-	mv	a1, t2
-	li	a2, 0
-	ecall
+#	li	a7, 62		# this part seeks to start of raster data
+#	mv	a0, s0
+#	mv	a1, t2
+#	li	a2, 0
+#	ecall
 	
 	#li	a7, 63		# read pixels to heap memory pointed by t5
 	#mv	a0, s0
@@ -559,11 +540,11 @@ getPixles:
 # --------------------------------------------------
 
 # --------------------------------------------------
-getBitsPerPixels:
+#getBitsPerPixels:
 	# needs   <- address to generic image data in a0
 	# returns -> bits per pixel in a1
-	lhu	a1, 28(a0)
-	ret
+#	lhu	a1, 28(a0)
+#	ret
 # --------------------------------------------------
 
 # --------------------------------------------------
@@ -618,25 +599,25 @@ formatStaticImgData:
 	sh	a0, 28(t3)
 	
 	# ----------------------- this part sets used colors count
-	la	t3, fileDt
-	sh	t1, 46(t3)
-	srli	t1, t1, 16
-	sh	t1, 48(t3)
+#	la	t3, fileDt
+#	sh	t1, 46(t3)
+#	srli	t1, t1, 16
+#	sh	t1, 48(t3)
 	
-	mv	a0, t2
+#	mv	a0, t2
 	mv	a1, s9	
 	slli	a1, a1, 1	# mul pixel by 3 (each pixel takes 3 bytes in 24bit color space)
 	add	a1, a1, s9	# --||--
-	li	a0, 0
+#	li	a0, 0
 
 	ret
 # --------------------------------------------------
 
 # --------------------------------------------------
 saveImg:
-	# needs   <- colorsTable size in a0, rasterData size in a1
+	# needs   <- rasterData size in a1
 	# returns -> NOTHING
-	mv	t0, a0
+#	mv	t0, 0
 	mv	t1, a1
 
 	li	a7, 57		# this part closes read-only file
